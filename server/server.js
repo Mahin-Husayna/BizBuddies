0fa9451b410cdb1043a3755bd6ac5f37b963c85e
Mahin Husayna <mahinhusayna1044@gmail.com>
2026-04-24 16:46:18 +0600

    MongoDB connected

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
@@ -7,6 +8,11 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected ✅"))
+  .catch((err) => console.log("MongoDB error ❌:", err));
 
 // Test route
 app.get("/", (req, res) => {
